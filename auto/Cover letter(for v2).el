0678bae28cf3356dbc221b57e0264be25b8a9b58
Peter <pjb9ub@virginia.edu>
2020-02-09 13:19:21 -0500

    merge2

---
+++ auto/Cover letter(for v2).el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook
+ "Cover letter(for v2)"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "commands"
+    "article"
+    "art10"
+    "url"
+    "hyperref"
+    "xr"
+    "tikz")
+   (LaTeX-add-labels
+    "MONDEFCOR COR")
+   (LaTeX-add-bibliographies
+    "biblio"))
+ :latex)
+
